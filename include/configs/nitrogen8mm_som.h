64784df15469b8d83f0c9b1adf09721f20fb1915
Christoph Prater <christoph.prater@navimatix.de>
2020-02-20 13:14:19 +0100

    Switch U-Boot console to UART4
    UART2 and UART4 have been mixed for board featherback, that's
    why the debug UART is available on the wrong connector.
    Fix that by changing the default debug UART.
    
    NGSH-55
    
    Change-Id: I4050ac3963a53176c1fed8f220d2cc02a8d3b293
    Reviewed-on: http://code.lumira.mp-labs.de/c/extern/u-boot-imx6/+/2205
    Reviewed-by: Stephan Linz <stephan.linz@navimatix.de>
    Tested-by: Christoph Prater <christoph.prater@navimatix.de>
    Tested-by: Stephan Linz <stephan.linz@navimatix.de>

--- include/configs/nitrogen8mm_som.h
+++ include/configs/nitrogen8mm_som.h
@@ -134,7 +134,7 @@
 
 #define CONFIG_BAUDRATE			115200
 
-#define CONFIG_MXC_UART_BASE		UART2_BASE_ADDR
+#define CONFIG_MXC_UART_BASE		UART4_BASE_ADDR
 
 /* Monitor Command Prompt */
 #define CONFIG_SYS_PROMPT_HUSH_PS2     "> "
@@ -202,6 +202,10 @@
 #define BD_CONSOLE	"ttymxc1"
 #elif CONFIG_MXC_UART_BASE == UART1_BASE_ADDR
 #define BD_CONSOLE	"ttymxc0"
+#elif CONFIG_MXC_UART_BASE == UART3_BASE_ADDR
+#define BD_CONSOLE	"ttymxc2"
+#elif CONFIG_MXC_UART_BASE == UART4_BASE_ADDR
+#define BD_CONSOLE	"ttymxc3"
 #endif
 #endif
 
